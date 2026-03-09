4a733331d2fe9be846872c70fdb268118dacfe57
zakazbot01 <zakazbot01@gmail.com>
2026-03-09 22:38:01 +0300

    Create __init__.py

---
+++ keyboards/__init__.py
@@ -0,0 +1,10 @@
+# keyboards/__init__.py
+from .inline import (
+    destination_kb,
+    confirm_kb,
+    my_links_kb,
+    link_actions_kb,
+    reveal_in_channel_kb,
+    question_actions_kb,
+    back_button
+)
